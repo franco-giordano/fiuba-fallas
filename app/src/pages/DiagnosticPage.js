b703b355fb3d3ef352efc4b3234ff8bdb162a9f2
Santiago Mariani <smariani@devartis.com>
2022-06-27 01:10:11 -0300

    added functionality to go back

--- app/src/pages/DiagnosticPage.js
+++ app/src/pages/DiagnosticPage.js
@@ -21,124 +21,126 @@ function DiagnosticPage() {
 
 	/* SINTOMAS */
 	const [state, setState] = React.useState({
-			tos: false,
-			fiebre: false,
-			mocos: false,
-			dificultadRespiratoria: false,
-			dolorMuscular: false,
-			decaimiento: false,
-			diarrea: false,
-			vomitos: false,
+		tos: false,
+		fiebre: false,
+		mocos: false,
+		dificultadRespiratoria: false,
+		dolorMuscular: false,
+		decaimiento: false,
+		diarrea: false,
+		vomitos: false,
+	});
+
+	const handleSymptomsChange = (event) => {
+		setState({
+			...state,
+			[event.target.name]: event.target.checked,
 		});
-	
-		const handleSymptomsChange = (event) => {
-			setState({
-				...state,
-				[event.target.name]: event.target.checked,
-			});
-		};
-	
-		const { 
-			tos,
-			fiebre,
-			mocos,
-			dificultadRespiratoria,
-			dolorMuscular,
-			decaimiento,
-			diarrea,
-			vomitos,
-		} = state;
+	};
 
-		/* ESTADO PACIENTE */
+	const {
+		tos,
+		fiebre,
+		mocos,
+		dificultadRespiratoria,
+		dolorMuscular,
+		decaimiento,
+		diarrea,
+		vomitos,
+	} = state;
 
-		const [clinicState, setClinicState] = React.useState('ESTABLE');
+	/* ESTADO PACIENTE */
 
-		const handleClinicStateChange = (event) => {
-			setClinicState((event.target).value);
-		};
-		
-		/* RESULTADO COVID */
+	const [clinicState, setClinicState] = React.useState('ESTABLE');
 
-		const [covidTestResultState, setCovidTestResultState] = React.useState('NEGATIVO');
+	const handleClinicStateChange = (event) => {
+		setClinicState((event.target).value);
+	};
 
-		const handleCovidTestResultChange = (event) => {
-			setCovidTestResultState((event.target).value);
-		};
-		
+	/* RESULTADO COVID */
 
-		/* RESULTADOS */
+	const [covidTestResultState, setCovidTestResultState] = React.useState('NEGATIVO');
 
-		const paciente = [
-			"Indicar aislamiento",
-			"Monitorear evolución"
-		];
-				
-		const contactosEstrechos = [
-			"Indicar aislamiento"
-		];
+	const handleCovidTestResultChange = (event) => {
+		setCovidTestResultState((event.target).value);
+	};
 
-    return (
-			/*
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-        <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-          <FormLabel component="legend">¿Que sintomas tiene el paciente?</FormLabel>
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox checked={tos} onChange={handleSymptomsChange} name="tos" />
-              }
-              label="Tos"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox checked={fiebre} onChange={handleSymptomsChange} name="fiebre" />
-              }
-              label="Fiebre"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox checked={mocos} onChange={handleSymptomsChange} name="mocos" />
-              }
-              label="Mocos"
-            />
+
+	/* RESULTADOS */
+
+	const paciente = [
+		"Indicar aislamiento",
+		"Monitorear evolución"
+	];
+
+	const contactosEstrechos = [
+		"Indicar aislamiento"
+	];
+
+	const [results, setResults] = React.useState(true);
+
+	if (!results) {
+		return (
+			<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+				<FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+					<FormLabel component="legend">¿Que sintomas tiene el paciente?</FormLabel>
+					<FormGroup>
 						<FormControlLabel
-              control={
-                <Checkbox checked={dificultadRespiratoria} onChange={handleSymptomsChange} name="dificultadRespiratoria" />
-              }
-              label="Dificultad respiratoria"
-            />
+							control={
+								<Checkbox checked={tos} onChange={handleSymptomsChange} name="tos" />
+							}
+							label="Tos"
+						/>
 						<FormControlLabel
-              control={
-                <Checkbox checked={dolorMuscular} onChange={handleSymptomsChange} name="dolorMuscular" />
-              }
-              label="Dolor muscular"
-            />
+							control={
+								<Checkbox checked={fiebre} onChange={handleSymptomsChange} name="fiebre" />
+							}
+							label="Fiebre"
+						/>
 						<FormControlLabel
-              control={
-                <Checkbox checked={decaimiento} onChange={handleSymptomsChange} name="decaimiento" />
-              }
-              label="Decaimiento"
-            />
+							control={
+								<Checkbox checked={mocos} onChange={handleSymptomsChange} name="mocos" />
+							}
+							label="Mocos"
+						/>
 						<FormControlLabel
-              control={
-                <Checkbox checked={diarrea} onChange={handleSymptomsChange} name="diarrea" />
-              }
-              label="Diarrea"
-            />
+							control={
+								<Checkbox checked={dificultadRespiratoria} onChange={handleSymptomsChange} name="dificultadRespiratoria" />
+							}
+							label="Dificultad respiratoria"
+						/>
 						<FormControlLabel
-              control={
-                <Checkbox checked={vomitos} onChange={handleSymptomsChange} name="vomitos" />
-              }
-              label="Vómitos"
-            />
-          </FormGroup>
+							control={
+								<Checkbox checked={dolorMuscular} onChange={handleSymptomsChange} name="dolorMuscular" />
+							}
+							label="Dolor muscular"
+						/>
+						<FormControlLabel
+							control={
+								<Checkbox checked={decaimiento} onChange={handleSymptomsChange} name="decaimiento" />
+							}
+							label="Decaimiento"
+						/>
+						<FormControlLabel
+							control={
+								<Checkbox checked={diarrea} onChange={handleSymptomsChange} name="diarrea" />
+							}
+							label="Diarrea"
+						/>
+						<FormControlLabel
+							control={
+								<Checkbox checked={vomitos} onChange={handleSymptomsChange} name="vomitos" />
+							}
+							label="Vómitos"
+						/>
+					</FormGroup>
 
 					<FormLabel id="demo-radio-buttons-group-label">¿Cual es el estado clínico del paciente?</FormLabel>
 					<RadioGroup
 						aria-labelledby="demo-radio-buttons-group-label"
 						name="radio-buttons-group"
 						value={clinicState}
-        		onChange={handleClinicStateChange}
+						onChange={handleClinicStateChange}
 					>
 						<FormControlLabel value="ESTABLE" control={<Radio />} label="Estable" />
 						<FormControlLabel value="DE_GRAVEDAD" control={<Radio />} label="De gravedad" />
@@ -149,7 +151,7 @@ function DiagnosticPage() {
 						aria-labelledby="demo-radio-buttons-group-label"
 						name="radio-buttons-group"
 						value={covidTestResultState}
-        		onChange={handleCovidTestResultChange}
+						onChange={handleCovidTestResultChange}
 					>
 						<FormControlLabel value="NEGATIVO" control={<Radio />} label="Negativo" />
 						<FormControlLabel value="POSITIVO" control={<Radio />} label="Positivo" />
@@ -160,54 +162,57 @@ function DiagnosticPage() {
 					<Button variant="contained" endIcon={<SendIcon />}>
 						Enviar
 					</Button>
-        </FormControl>
-    </Box>
-		*/
-		<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+				</FormControl>
+			</Box>
+		);
+	} else {
+		return (
 			<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-				<h1>¿Como continuar?</h1>
-				<Box sx={{ width: '300px'}}>
-					<h2>Paciente</h2>
-					<List>
-						{ paciente.map((value, index) => (
-							<ListItem key={index}>
-								<ListItemText
-									primary={value}
-								/>
-							</ListItem>
-						))}
-					</List>
-				</Box>
+				<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+					<h1>¿Como continuar?</h1>
+					<Box sx={{ width: '300px' }}>
+						<h2>Paciente</h2>
+						<List>
+							{paciente.map((value, index) => (
+								<ListItem key={index}>
+									<ListItemText
+										primary={value}
+									/>
+								</ListItem>
+							))}
+						</List>
+					</Box>
 
-				<Box sx={{ width: '300px'}}>
-					<h2>Contactos Estrechos</h2>
-					<List>
-						{ contactosEstrechos.map((value, index) => (
-							<ListItem key={index}>
-								<ListItemText
-									primary={value}
-								/>
-							</ListItem>
-						))}
-					</List>
+					<Box sx={{ width: '300px' }}>
+						<h2>Contactos Estrechos</h2>
+						<List>
+							{contactosEstrechos.map((value, index) => (
+								<ListItem key={index}>
+									<ListItemText
+										primary={value}
+									/>
+								</ListItem>
+							))}
+						</List>
+					</Box>
+				</Box>
+				<Box sx={{
+					display: 'flex',
+					flexDirection: 'row',
+					alignItems: 'start',
+					columnGap: '15px',
+				}}>
+					<Button variant="contained" onClick={()=>(setResults(false))}>
+						Realizar otro diagnostico
+					</Button>
+
+					<Button variant="contained">
+						Exportar resultado
+					</Button>
 				</Box>
 			</Box>
-			<Box sx={{ 
-				display: 'flex',
-				flexDirection: 'row',
-				alignItems: 'start',
-				columnGap: '15px',
-			}}>
-				<Button variant="contained">
-				Realizar otro diagnostico
-				</Button>
+		);
+	}
+}
 
-				<Button variant="contained">
-				Exportar resultado
-				</Button>
-			</Box>
-		</Box>
-    );
-  }
-  
-  export default DiagnosticPage;
+export default DiagnosticPage;
